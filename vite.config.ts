0ee4758205c3852ecc0f63a225a36fabb1d0ea8f
陈刑 <dev-chenxing.io@outlook.com>
2025-07-16 17:24:20 +0800

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		host: '0.0.0.0',
-	},
-	base: "/drink-tier-list",
-	plugins: [
-		tailwindcss(),
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-			},
-		}),
-	],
+  base: "/drink-tier-list",
+  plugins: [
+    tailwindcss(),
+    preact({
+      prerender: {
+        enabled: true,
+        renderTarget: "#app"
+      }
+    })
+  ]
 });
