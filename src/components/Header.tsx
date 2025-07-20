40b3d140dbb1bb4583ebd59e87fa65b318fbd9b7
chenxing-dev <dev.chenxing@gmail.com>
2025-07-20 11:18:26 +0800

    logo.svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Drink } from "../types";
+import LazyImage from "./LazyImage";
 
 export default function Header({ drinks }: { drinks: Drink[] }) {
   // 统计品牌数量
@@ -8,10 +9,14 @@ export default function Header({ drinks }: { drinks: Drink[] }) {
     <header className="bg-white border-b-4 border-black px-6 py-3">
       <div className="flex flex-row justify-between items-center gap-3">
         {/* 左侧标题区域 */}
-        <div className="flex items-baseline gap-3">
-          <h1 className="text-3xl md:text-4xl font-bold text-black tracking-tight">饮料 Tier List</h1>
+        <div className="flex items-end gap-2">
+          {/* Logo */}
+          <LazyImage src="logo.svg" alt="Logo" className="w-12 h-12" />
+
+          {/* 文字LOGO */}
+          <h1 className="text-3xl md:text-4xl font-bold text-black tracking-tight mb-0.5">饮料 Tier List</h1>
           {/* 移动端隐藏的副标题 */}
-          <p className="hidden md:block text-neutral-700 font-medium italic border-l-2 border-black pl-3">好喝难喝，喝过才敢评</p>
+          <p className="hidden md:block text-neutral-700 font-medium italic border-l-2 border-black ml-2 pl-3 leading-tight mb-1">好喝难喝，喝过才敢评</p>
         </div>
         {/* 统计信息 */}
         <div className="flex gap-2">
